7d38e68bb192d149fefa9041257d3fbb4a52cf6e
Karl Heinz Marbaise <kama@soebes.de>
2019-08-06 13:34:38 +0200

    Removed Nested Classes.

--- src/test/java/com/soebes/precondition/PreconditionTest.java
+++ src/test/java/com/soebes/precondition/PreconditionTest.java
@@ -3,8 +3,6 @@ package com.soebes.precondition;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -14,63 +12,53 @@ import org.junit.jupiter.api.Test;
  */
 class PreconditionTest {
 
-  @Nested
-  @DisplayName("requireGreaterThanZero for Long parameter Tests")
-  class RequireGreaterThanZeroLongTests {
-
-    @Test
-    void requireGreaterThanZeroIAE() {
-      assertThatIllegalArgumentException().isThrownBy(
-          () -> Precondition.requireGreaterThanZero((Long) null, "Null Message"));
-    }
-
-    @Test
-    void requireGreaterThanZeroIAEWithGivenMessage() {
-      assertThatIllegalArgumentException().isThrownBy(
-          () -> Precondition.requireGreaterThanZero((Long) null, "The given Message")).withMessage("The given Message");
-    }
-
-    @Test
-    void requireGreaterThanZeroWithZeroValue() {
-      assertThatIllegalArgumentException().isThrownBy(() -> Precondition.requireGreaterThanZero(0L, "The Zero Message"))
-          .withMessage("The Zero Message");
-    }
-
-    @Test
-    void should_return_same_object() {
-      Long longValue = Long.MAX_VALUE;
-      assertThat(Precondition.requireGreaterThanZero(longValue, "The failure message")).isSameAs(longValue);
-    }
+  @Test
+  void requireGreaterThanZeroIAE() {
+    assertThatIllegalArgumentException().isThrownBy(
+        () -> Precondition.requireGreaterThanZero((Long) null, "Null Message"));
   }
 
-  @Nested
-  @DisplayName("requireGreaterThanZero for Integer parameter Tests")
-  class RequireGreaterThanZeroIntegerTests {
+  @Test
+  void requireGreaterThanZeroIAEWithGivenMessage() {
+    assertThatIllegalArgumentException().isThrownBy(
+        () -> Precondition.requireGreaterThanZero((Long) null, "The given Message")).withMessage("The given Message");
+  }
 
-    @Test
-    void requireGreaterThanZeroIAE() {
-      assertThatIllegalArgumentException().isThrownBy(
-          () -> Precondition.requireGreaterThanZeroInteger((Integer) null, "The Null Message"));
-    }
+  @Test
+  void requireGreaterThanZeroWithZeroValue() {
+    assertThatIllegalArgumentException().isThrownBy(() -> Precondition.requireGreaterThanZero(0L, "The Zero Message"))
+        .withMessage("The Zero Message");
+  }
 
-    @Test
-    void requireGreaterThanZeroIAEWithGivenMessage() {
-      assertThatIllegalArgumentException().isThrownBy(
-          () -> Precondition.requireGreaterThanZeroInteger((Integer) null, "The Null Message"))
-          .withMessage("The Null Message");
-    }
+  @Test
+  void should_return_same_object() {
+    Long longValue = Long.MAX_VALUE;
+    assertThat(Precondition.requireGreaterThanZero(longValue, "The failure message")).isSameAs(longValue);
+  }
 
-    @Test
-    void requireGreaterThanZeroWithZeroValue() {
-      assertThatIllegalArgumentException().isThrownBy(
-          () -> Precondition.requireGreaterThanZeroInteger(Integer.valueOf(0), "The Zero Message"))
-          .withMessage("The Zero Message");
-    }
+  @Test
+  void requireGreaterThanZeroIAEInteger() {
+    assertThatIllegalArgumentException().isThrownBy(
+        () -> Precondition.requireGreaterThanZeroInteger((Integer) null, "The Null Message"));
+  }
 
-    @Test
-    void should_return_same_object() {
-      Integer integerValue = Integer.MAX_VALUE;
-      assertThat(Precondition.requireGreaterThanZeroInteger(integerValue, "The failure message")).isSameAs(integerValue);
-    }
+  @Test
+  void requireGreaterThanZeroIAEWithGivenMessageInteger() {
+    assertThatIllegalArgumentException().isThrownBy(
+        () -> Precondition.requireGreaterThanZeroInteger((Integer) null, "The Null Message"))
+        .withMessage("The Null Message");
+  }
+
+  @Test
+  void requireGreaterThanZeroWithZeroValueInteger() {
+    assertThatIllegalArgumentException().isThrownBy(
+        () -> Precondition.requireGreaterThanZeroInteger(Integer.valueOf(0), "The Zero Message"))
+        .withMessage("The Zero Message");
+  }
+
+  @Test
+  void should_return_same_objectInteger() {
+    Integer integerValue = Integer.MAX_VALUE;
+    assertThat(Precondition.requireGreaterThanZeroInteger(integerValue, "The failure message")).isSameAs(integerValue);
   }
 }
