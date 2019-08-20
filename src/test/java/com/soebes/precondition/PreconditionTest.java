43593838303384903c32caefcd30fb8094a79f55
Karl Heinz Marbaise <kama@soebes.de>
2019-08-20 19:50:29 +0200

    Using static import.

--- src/test/java/com/soebes/precondition/PreconditionTest.java
+++ src/test/java/com/soebes/precondition/PreconditionTest.java
@@ -1,5 +1,7 @@
 package com.soebes.precondition;
 
+import static com.soebes.precondition.Precondition.requireGreaterThanZero;
+import static com.soebes.precondition.Precondition.requireGreaterThanZeroInteger;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
@@ -21,25 +23,25 @@ class PreconditionTest {
     @Test
     void requireGreaterThanZeroIAE() {
       assertThatIllegalArgumentException().isThrownBy(
-          () -> Precondition.requireGreaterThanZero((Long) null, "Null Message"));
+          () -> requireGreaterThanZero((Long) null, "Null Message"));
     }
 
     @Test
     void requireGreaterThanZeroIAEWithGivenMessage() {
       assertThatIllegalArgumentException().isThrownBy(
-          () -> Precondition.requireGreaterThanZero((Long) null, "The given Message")).withMessage("The given Message");
+          () -> requireGreaterThanZero((Long) null, "The given Message")).withMessage("The given Message");
     }
 
     @Test
     void requireGreaterThanZeroWithZeroValue() {
-      assertThatIllegalArgumentException().isThrownBy(() -> Precondition.requireGreaterThanZero(0L, "The Zero Message"))
+      assertThatIllegalArgumentException().isThrownBy(() -> requireGreaterThanZero(0L, "The Zero Message"))
           .withMessage("The Zero Message");
     }
 
     @Test
     void should_return_same_object() {
       Long longValue = Long.MAX_VALUE;
-      assertThat(Precondition.requireGreaterThanZero(longValue, "The failure message")).isSameAs(longValue);
+      assertThat(requireGreaterThanZero(longValue, "The failure message")).isSameAs(longValue);
     }
   }
 
@@ -50,27 +52,27 @@ class PreconditionTest {
     @Test
     void requireGreaterThanZeroIAE() {
       assertThatIllegalArgumentException().isThrownBy(
-          () -> Precondition.requireGreaterThanZeroInteger((Integer) null, "The Null Message"));
+          () -> requireGreaterThanZeroInteger((Integer) null, "The Null Message"));
     }
 
     @Test
     void requireGreaterThanZeroIAEWithGivenMessage() {
       assertThatIllegalArgumentException().isThrownBy(
-          () -> Precondition.requireGreaterThanZeroInteger((Integer) null, "The Null Message"))
+          () -> requireGreaterThanZeroInteger((Integer) null, "The Null Message"))
           .withMessage("The Null Message");
     }
 
     @Test
     void requireGreaterThanZeroWithZeroValue() {
       assertThatIllegalArgumentException().isThrownBy(
-          () -> Precondition.requireGreaterThanZeroInteger(Integer.valueOf(0), "The Zero Message"))
+          () -> requireGreaterThanZeroInteger(Integer.valueOf(0), "The Zero Message"))
           .withMessage("The Zero Message");
     }
 
     @Test
     void should_return_same_object() {
       Integer integerValue = Integer.MAX_VALUE;
-      assertThat(Precondition.requireGreaterThanZeroInteger(integerValue, "The failure message")).isSameAs(integerValue);
+      assertThat(requireGreaterThanZeroInteger(integerValue, "The failure message")).isSameAs(integerValue);
     }
   }
 }
